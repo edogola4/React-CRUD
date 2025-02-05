fecdaa4edb6917ad4d17a4f7be4e547f974fe2af
edogola4 <edogola4@gmail.com>
2025-02-05 21:52:26 +0300

    updated

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 function QuestionItem({ question }) {
   const { id, prompt, answers, correctIndex } = question;
@@ -18,6 +18,54 @@ function QuestionItem({ question }) {
         <select defaultValue={correctIndex}>{options}</select>
       </label>
       <button>Delete Question</button>
+    </li>
+  );
+}
+
+export default QuestionItem;
+*/
+
+
+
+import React from "react";
+
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
+  const { id, prompt, answers, correctIndex } = question;
+
+  const handleDeleteClick = () => {
+    onDeleteQuestion(id);
+  };
+
+  const handleCorrectAnswerChange = (e) => {
+    const newCorrectIndex = parseInt(e.target.value);
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex: newCorrectIndex }),
+    })
+      .then((res) => res.json())
+      .then((updatedQuestion) => {
+        onUpdateQuestion(updatedQuestion);
+      });
+  };
+
+  const options = answers.map((answer, index) => (
+    <option key={index} value={index}>
+      {answer}
+    </option>
+  ));
+
+  return (
+    <li>
+      <h4>Question {id}</h4>
+      <h5>{prompt}</h5>
+      <label>
+        Correct Answer:
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
+      </label>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
